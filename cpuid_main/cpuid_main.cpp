04c54ebd72fc1f58230f0a436c4bf77195b466cf
jrmwng <jmw@netvigator.com>
2016-02-07 02:25:20 +0800

    check features

--- cpuid_main/cpuid_main.cpp
+++ cpuid_main/cpuid_main.cpp
@@ -7,10 +7,12 @@
 int main()
 {
 	// execution CPUID instructions
+
 	jrmwng::cpuid_tree_t<0x17> CPUID;
 	jrmwng::cpuid_tree_t<0x80000008> ExtendedCPUID;
 
 	// output CPUID data to the file "cpuid.txt"
+
 	std::ofstream ofs("cpuid.txt");
 	{
 		ofs << CPUID << ExtendedCPUID;
@@ -18,10 +20,12 @@ int main()
 	}
 
 	// zero data structures
+
 	memset(&CPUID, 0, sizeof(CPUID));
 	memset(&ExtendedCPUID, 0, sizeof(ExtendedCPUID));
 
 	// input CPUID data from the file "cpuid.txt"
+
 	std::ifstream ifs("cpuid.txt");
 	{
 		ifs >> CPUID >> ExtendedCPUID;
@@ -29,6 +33,41 @@ int main()
 	}
 
 	// print CPUID data
+
 	std::cout << CPUID << ExtendedCPUID;
+
+	// print features
+
+	if (CPUID.uFPU)
+		std::cout << "FPU is available" << std::endl;
+	if (CPUID.uMMX)
+		std::cout << "MMX is available" << std::endl;
+
+	if (CPUID.uSSE && CPUID.uSSE2 && CPUID.uSSE3 && CPUID.uSSSE3 && CPUID.uSSE4_1 && CPUID.uSSE4_2)
+		std::cout << "SSE is available (all)" << std::endl;
+	else if (CPUID.uSSE || CPUID.uSSE2 || CPUID.uSSE3 || CPUID.uSSSE3 || CPUID.uSSE4_1 || CPUID.uSSE4_2)
+		std::cout << "SSE is available (some)" << std::endl;
+
+	if (CPUID.uAVX && CPUID.uAVX2)
+		std::cout << "AVX is available (all)" << std::endl;
+	else if (CPUID.uAVX || CPUID.uAVX2)
+		std::cout << "AVX is available (some)" << std::endl;
+
+	if (CPUID.uBMI1 && CPUID.uBMI2)
+		std::cout << "BMI is available (all)" << std::endl;
+	else if (CPUID.uBMI1 || CPUID.uBMI2)
+		std::cout << "BMI is available (some)" << std::endl;
+
+	if (CPUID.uHLE)
+		std::cout << "HLE is available" << std::endl;
+	if (CPUID.uRTM)
+		std::cout << "RTM is available" << std::endl;
+
+	if (CPUID.uIntelMPX)
+		std::cout << "Intel MPX is available" << std::endl;
+
+	if (CPUID.uIntelSGX)
+		std::cout << "Intel SGX is available" << std::endl;
+
 	return 0;
 }
